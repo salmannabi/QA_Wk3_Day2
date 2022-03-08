99f3aef291f90228f6764cad915b9751bfd74b37
Salman <salmannabi183@yahoo.com>
2022-03-08 14:55:03 +0000

    Initital Commit

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+
+	public void helloWorld() {
+		System.out.println("Hello World!");
+	}
+	
+	public void printMessage(String x, String y) {
+		
+	}
+}
---
+++ src/com/qa/helloworld/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.helloworld;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld myObj = new HelloWorld();
+		
+		myObj.helloWorld();
+		myObj.printMessage("Hello World", "This is some text");
+	}
+	
+}
